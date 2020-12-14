5d1a2da2b1e49f0e2d2dc7df5b820aea48515992
andersonsaj <andersonsb09@hotmail.com>
2020-12-14 12:53:27 -0300

    API Pronta

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import authConfig from '../config/auth';
+import AppError from "../errors/AppError";
+
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+export default function ensureAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction
+): void {
+    //Validação do token jwt
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError('JWT token is missing', 401);
+    }
+    // Bearer token vai ser dividido
+    const [, token] = authHeader.split(' ');
+    try {
+        const decoded = verify(token, authConfig.jwt.secret);
+        
+        const { sub } = decoded as TokenPayload;
+        
+        request.user = {
+            id: sub,
+        };
+
+        return next();
+    } catch {
+        throw new AppError('Invalid JWT token', 401);
+    }
+
+}
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,63 @@
+import { Router } from 'express';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import CreateUserService from '../services/CreateUsersService';
+import uploadConfig from '../config/upload';
+import multer from 'multer';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const userResponse = await createUser.execute({
+            name,
+            email,
+            password,
+        });
+
+        //  delete user.password;
+        const user = {
+            id: userResponse.id,
+            name: userResponse.name,
+            email: userResponse.email,
+            created_at: userResponse.created_at,
+            updated_at: userResponse.updated_at,
+        }
+        return response.json(user);
+    }
+    catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        });
+
+        const userResponse = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        }
+
+        return response.json(userResponse);
+    });
+
+export default usersRouter;   
