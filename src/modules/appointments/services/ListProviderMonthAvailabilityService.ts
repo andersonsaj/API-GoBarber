0fef221074c67eb0cbef50008a58801d36275a48
andersonsaj <andersonsb09@hotmail.com>
2020-12-21 19:26:51 -0300

    Listando horário disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,3 +1,4 @@
+import { getDate, getDaysInMonth } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -31,8 +32,24 @@ class ListProviderMonthAvailabilityService {
         year,
       },
     );
-    console.log(appointments);
-    return [{ day: 1, available: false }];
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+    return availability;
   }
 }
 
