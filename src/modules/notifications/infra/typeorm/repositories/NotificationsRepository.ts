15607fa159752144482c9e924449585460ad4c45
andersonsaj <andersonsb09@hotmail.com>
2020-12-22 17:23:55 -0300

    Configurando MongoDB, Estrutura de notificações

---
+++ src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,26 @@
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import INotficationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import { getMongoRepository, MongoRepository } from 'typeorm';
+import Notification from '../schemas/Notification';
+
+export default class NoticationsRepository implements INotficationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    content,
+    recipient_id,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      content,
+      recipient_id,
+    });
+
+    await this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
