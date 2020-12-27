8b69f6ed3cc3cc5c38f3407dc93761cbd8f297b6
andersonsaj <andersonsb09@hotmail.com>
2020-12-27 15:47:31 -0300

    Clientes dos agendamentos

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -56,6 +56,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
     return appointments;
   }
